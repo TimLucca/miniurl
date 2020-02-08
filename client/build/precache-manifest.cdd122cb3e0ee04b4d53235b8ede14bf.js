a283162b5787b036d578a84df3a4242ae121fb55
Tim Lucca <tim.lucca88@gmail.com>
2020-02-07 19:02:05 -0700

    removed build from .gitignore so it can be pushed to heroku

---
+++ client/build/precache-manifest.cdd122cb3e0ee04b4d53235b8ede14bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae28623633cf478a1ad7f73659f67d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adbfd77af21de984230e",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "aeb62f8d66b5b54aa71d",
+    "url": "/static/js/2.e41f2005.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.e41f2005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbfd77af21de984230e",
+    "url": "/static/js/main.240328a6.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
